dcbc10986f69a91e637f813410721d9a81f9eef5
Nikhil Barik <nklmarch17@gmail.com>
2024-06-09 14:11:30 +0530

    Initial Commit

---
+++ battledeck/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  battledeck
+//
+//  Created by Nikhil Barik on 09/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ battledeck/battledeckApp.swift
@@ -0,0 +1,17 @@
+//
+//  battledeckApp.swift
+//  battledeck
+//
+//  Created by Nikhil Barik on 09/06/24.
+//
+
+import SwiftUI
+
+@main
+struct battledeckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
